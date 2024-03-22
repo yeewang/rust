4a2f0d4229bef1adbb763eed9ebd8ee8ee38932c
Yi Wang <wangyi8848@gmail.com>
2024-03-22 19:09:15 +0800

    Change the env from ohos to musl

--- compiler/rustc_target/src/spec/linux_ohos_base.rs
+++ compiler/rustc_target/src/spec/linux_ohos_base.rs
@@ -4,7 +4,7 @@ use crate::spec::TargetOptions;
 pub fn opts() -> TargetOptions {
     let mut base = super::linux_base::opts();
 
-    base.env = "ohos".into();
+    base.env = "musl".into();
     base.pre_link_objects_self_contained = crt_objects::pre_musl_self_contained();
     base.post_link_objects_self_contained = crt_objects::post_musl_self_contained();
     base.link_self_contained = LinkSelfContainedDefault::Musl;
